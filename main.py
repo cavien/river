8b0d8b48e05e966271aec1f04239eaee92c34b23
cavien <Cavien@qq.com>
2025-12-24 10:28:27 +0800

    modify main

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 def cavienadd(a, b):
-    """
-    Add two numbers.
-    
-    Args:
-        a (int): The first number
-        b (int): The second number
-        
-    Returns:
-        int: The sum of the two numbers
-    """
-    return a + b
+    pass
 def river():
     print("river")
     
